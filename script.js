3b862526af5090f7d188a1ce45960f99bf57b091
Glitch (local-verbose-alfalfa) <none>
2021-11-05 13:42:38 +0000

    👫💘 Updated with Glitch

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+document.createElement("note");
 function install() {
 var URI = document.getElementById("URI").value;
   window.location.replace('itms-services://?action=download-manifest&url='+URI);
