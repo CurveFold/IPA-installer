d424d98a55e6714e091f137fa4552a54cefb00fa
Sonik <69720319+SuperS0n1k@users.noreply.github.com>
2021-11-05 16:00:41 +0000

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,7 @@ function install() {
 var URI = document.getElementById("URI").value;
   window.location.replace('itms-services://?action=download-manifest&url='+URI);
 } 
+
+function unc0ver() {
+document.getElementById('myTextarea').value = 'https://unc0ver.dev/downloads/7.0.2/11c3f7b0eea806ba3cac975f74dcc2cc4e916bac/unc0ver_Release_7.0.2.ipa';
+}
