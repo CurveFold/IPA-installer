380d58b3e346d1852ee94b134bfbd38c09819f44
Sonik <69720319+SuperS0n1k@users.noreply.github.com>
2021-11-06 11:21:45 +0000

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,3 +7,13 @@ var URI = document.getElementById("URI").value;
 function unc0ver() {
 document.getElementById('URI').value = 'https://unc0ver.dev/downloads/7.0.2/11c3f7b0eea806ba3cac975f74dcc2cc4e916bac/unc0ver_Release_7.0.2.ipa';
 }
+document.getelementbyclassname("app").innerHTML = "<h1>IPA installer</h1>
+</h1>
+<center><textarea id="URI" placeholder="https://example.com/app.ipa"></textarea><button href="#" onclick="install()"><b>GET</b></button>
+  <h1></h1></center>
+<center ><note>note:this does not sign the ipa</note></center>
+<h5>suggested</h5>
+<a href="#" onclick="unc0ver()">unc0ver</a>
+<h1></h1>
+<footer><center ><note>VaultDevs © 2020</note></center></footer>
+"
